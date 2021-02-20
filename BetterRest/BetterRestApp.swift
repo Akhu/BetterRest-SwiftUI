9cb69e4cc59c6d77d5c7acf869d08cad32a58164
Anthony <me@anthony-dacruz.com>
2021-02-20 22:33:34 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anthony Da cruz on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
